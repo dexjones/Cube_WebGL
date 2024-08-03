d7dabe7c99624bea4f81a2e342d85d5886f7eb36
Dexter <dexter.t.jones@outlook.com>
2024-08-03 11:49:23 -0600

    Branch to utilize Painter's algorithm for the hidden-surface removal problem

--- app.js
+++ app.js
@@ -4,27 +4,67 @@ var vertexShaderText =
 '',
 'attribute vec3 vertPosition;', // Input vertex position
 'attribute vec3 vertColor;', // Input vertex color
+'attribute vec3 vertNormal;', // Input vertex normal
 'varying vec3 fragColor;', // Output color to fragment shader
+'varying vec3 vNormal;', // Pass normal to fragment shader
+'varying vec3 vPosition;', // Pass position to fragment shader
+'varying vec3 lightDirection;', // Pass light direction to fragment shader
 'uniform mat4 mWorld;', // World transformation matrix
 'uniform mat4 mView;', // View transformation matrix
 'uniform mat4 mProj;', // Projection transformation matrix
+'uniform vec4 uLightPosition;', // Light position in world space
 '',
 'void main()',
 '{',
 '   fragColor = vertColor;',
-'   gl_Position = mProj * mView * mWorld * vec4(vertPosition, 1.0);',
+'   vNormal = (mWorld * vec4(vertNormal, 0.0)).xyz;', // Transform normal to world space
+'   vPosition = (mWorld * vec4(vertPosition, 1.0)).xyz;', // Transform position to world space
+'   vec4 transformedLightPosition = mView * uLightPosition;', // Transform light position to eye space
+'   lightDirection = normalize(transformedLightPosition.xyz - (mView * vec4(vPosition, 1.0)).xyz);',
+'   gl_Position = mProj * mView * vec4(vPosition, 1.0);',
 '}'
 ].join('\n');
 
-// Fragment Shader: Defines the colors
 var fragmentShaderText =
+[
+'precision mediump float;', // Set the precision for float variables
+'',
+'varying vec3 fragColor;', // Input color from vertex shader
+'varying vec3 vNormal;', // Input normal from vertex shader
+'varying vec3 vPosition;', // Input position from vertex shader
+'varying vec3 lightDirection;', // Input light direction from vertex shader
+'uniform vec4 uLightAmbient;', // Ambient light
+'uniform vec4 uLightDiffuse;', // Diffuse light
+'uniform vec4 uLightSpecular;', // Specular light
+'uniform vec4 uMaterialAmbient;', // Material ambient
+'uniform vec4 uMaterialDiffuse;', // Material diffuse
+'uniform vec4 uMaterialSpecular;', // Material specular
+'uniform float uShininess;', // Material shininess
+'',
+'void main()',
+'{',
+'   vec3 normal = normalize(vNormal);',
+'   float diff = max(dot(lightDirection, normal), 0.0);',
+'   vec4 diffuse = diff * uLightDiffuse * uMaterialDiffuse;',
+'   vec3 viewDir = normalize(-vPosition);',
+'   vec3 reflectDir = reflect(-lightDirection, normal);',
+'   float spec = pow(max(dot(viewDir, reflectDir), 0.0), uShininess);',
+'   vec4 specular = spec * uLightSpecular * uMaterialSpecular;',
+'   vec4 ambient = uLightAmbient * uMaterialAmbient;',
+'   vec4 color = ambient + diffuse + specular;',
+'   gl_FragColor = vec4(color.rgb * fragColor, 1.0);',
+'}'
+].join('\n');
+
+// Another fragment shader for toggling
+var fragmentShaderTextAlt =
 [
 'precision mediump float;', // Set the precision for float variables
 '',
 'varying vec3 fragColor;', // Input color from vertex shader
 'void main()',
 '{',
-'   gl_FragColor = vec4(fragColor, 1.0);', // Set the fragment color
+'   gl_FragColor = vec4(fragColor.r, fragColor.g, 1.0 - fragColor.b, 1.0);', // Different color effect
 '}'
 ].join('\n');
 
@@ -53,72 +93,70 @@ var InitDemo = function() {
     gl.viewport(0, 0, canvas.width, canvas.height);
 
     // Compile shaders
-    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    var compileShader = function(shaderText, shaderType) {
+        var shader = gl.createShader(shaderType);
+        gl.shaderSource(shader, shaderText);
+        gl.compileShader(shader);
+        if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+            console.error('ERROR compiling shader!', gl.getShaderInfoLog(shader));
+            return null;
+        }
+        return shader;
+    };
 
-    gl.shaderSource(vertexShader, vertexShaderText);
-    gl.shaderSource(fragmentShader, fragmentShaderText);
+    var vertexShader = compileShader(vertexShaderText, gl.VERTEX_SHADER);
+    var fragmentShader = compileShader(fragmentShaderText, gl.FRAGMENT_SHADER);
+    var fragmentShaderAlt = compileShader(fragmentShaderTextAlt, gl.FRAGMENT_SHADER);
 
-    gl.compileShader(vertexShader);
-    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-        console.error('ERROR compiling vertex shader!', gl.getShaderInfoLog(vertexShader));
-        return;
-    }
-    gl.compileShader(fragmentShader);
-    if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-        console.error('ERROR compiling fragment shader!', gl.getShaderInfoLog(fragmentShader));
-        return;
-    }
+    var createProgram = function(vertexShader, fragmentShader) {
+        var program = gl.createProgram();
+        gl.attachShader(program, vertexShader);
+        gl.attachShader(program, fragmentShader);
+        gl.linkProgram(program);
+        if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+            console.error('Error linking program!', gl.getProgramInfoLog(program));
+            return null;
+        }
+        return program;
+    };
 
-    // Link shaders into program
-    var program = gl.createProgram();
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    gl.linkProgram(program);
-    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-        console.error('Error linking program!', gl.getProgramInfoLog(program));
-        return;
-    }
-
-    gl.useProgram(program);
+    var program = createProgram(vertexShader, fragmentShader);
+    var programAlt = createProgram(vertexShader, fragmentShaderAlt);
+    var currentProgram = program;
+    gl.useProgram(currentProgram);
     
     // Define cube vertices and indices
     var boxVertices = 
     [
-        -1.0,  1.0, -1.0,   0.5, 0.5, 0.5,
-        -1.0,  1.0,  1.0,   0.5, 0.5, 0.5,
-         1.0,  1.0,  1.0,   0.5, 0.5, 0.5,
-         1.0,  1.0, -1.0,   0.5, 0.5, 0.5,
-
-        //Left
-        -1.0,  1.0,  1.0,   0.75, 0.25, 0.5,
-        -1.0, -1.0,  1.0,   0.75, 0.25, 0.5,
-        -1.0, -1.0, -1.0,   0.75, 0.25, 0.5,
-        -1.0,  1.0, -1.0,   0.75, 0.25, 0.5,
-
-        //Right
-        1.0,  1.0,  1.0,    0.25, 0.25, 0.75,
-        1.0, -1.0,  1.0,    0.25, 0.25, 0.75,
-        1.0, -1.0, -1.0,    0.25, 0.25, 0.75,
-        1.0,  1.0, -1.0,    0.25, 0.25, 0.75,
-
-        //Front
-         1.0, 1.0,  1.0,   1.0, 0.0, 0.15,
-         1.0,-1.0,  1.0,   1.0, 0.0, 0.15,
-        -1.0,-1.0,  1.0,   1.0, 0.0, 0.15,
-        -1.0, 1.0,  1.0,   1.0, 0.0, 0.15,
-
-        //Back
-         1.0,  1.0, -1.0,   0.0, 1.0, 0.15,
-         1.0, -1.0, -1.0,   0.0, 1.0, 0.15,
-        -1.0, -1.0, -1.0,   0.0, 1.0, 0.15,
-        -1.0,  1.0, -1.0,   0.0, 1.0, 0.15,
-
-        //Bottom
-        -1.0, -1.0,-1.0,   0.5, 0.5, 1.0,
-        -1.0, -1.0, 1.0,   0.5, 0.5, 1.0,
-         1.0, -1.0, 1.0,   0.5, 0.5, 1.0,
-         1.0, -1.0,-1.0,   0.5, 0.5, 1.0,
+        -1.0,  1.0, -1.0,   0.5, 0.5, 0.5,   0.0,  1.0,  0.0,
+        -1.0,  1.0,  1.0,   0.5, 0.5, 0.5,   0.0,  1.0,  0.0,
+         1.0,  1.0,  1.0,   0.5, 0.5, 0.5,   0.0,  1.0,  0.0,
+         1.0,  1.0, -1.0,   0.5, 0.5, 0.5,   0.0,  1.0,  0.0,
+        // Left
+        -1.0,  1.0,  1.0,   0.75, 0.25, 0.5,  -1.0,  0.0,  0.0,
+        -1.0, -1.0,  1.0,   0.75, 0.25, 0.5,  -1.0,  0.0,  0.0,
+        -1.0, -1.0, -1.0,   0.75, 0.25, 0.5,  -1.0,  0.0,  0.0,
+        -1.0,  1.0, -1.0,   0.75, 0.25, 0.5,  -1.0,  0.0,  0.0,
+        // Right
+        1.0,  1.0,  1.0,    0.25, 0.25, 0.75,  1.0,  0.0,  0.0,
+        1.0, -1.0,  1.0,    0.25, 0.25, 0.75,  1.0,  0.0,  0.0,
+        1.0, -1.0, -1.0,    0.25, 0.25, 0.75,  1.0,  0.0,  0.0,
+        1.0,  1.0, -1.0,    0.25, 0.25, 0.75,  1.0,  0.0,  0.0,
+        // Front
+         1.0, 1.0,  1.0,   1.0, 0.0, 0.15,   0.0,  0.0,  1.0,
+         1.0,-1.0,  1.0,   1.0, 0.0, 0.15,   0.0,  0.0,  1.0,
+        -1.0,-1.0,  1.0,   1.0, 0.0, 0.15,   0.0,  0.0,  1.0,
+        -1.0, 1.0,  1.0,   1.0, 0.0, 0.15,   0.0,  0.0,  1.0,
+        // Back
+         1.0,  1.0, -1.0,   0.0, 1.0, 0.15,   0.0,  0.0, -1.0,
+         1.0, -1.0, -1.0,   0.0, 1.0, 0.15,   0.0,  0.0, -1.0,
+        -1.0, -1.0, -1.0,   0.0, 1.0, 0.15,   0.0,  0.0, -1.0,
+        -1.0,  1.0, -1.0,   0.0, 1.0, 0.15,   0.0,  0.0, -1.0,
+        // Bottom
+        -1.0, -1.0,-1.0,   0.5, 0.5, 1.0,   0.0, -1.0,  0.0,
+        -1.0, -1.0, 1.0,   0.5, 0.5, 1.0,   0.0, -1.0,  0.0,
+         1.0, -1.0, 1.0,   0.5, 0.5, 1.0,   0.0, -1.0,  0.0,
+         1.0, -1.0,-1.0,   0.5, 0.5, 1.0,   0.0, -1.0,  0.0,
     ];
 
     var boxIndices = 
@@ -159,32 +197,64 @@ var InitDemo = function() {
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(boxIndices), gl.STATIC_DRAW);
 
     // Link vertex data to shader attributes
-    var positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
-    var colorAttribLocation = gl.getAttribLocation(program, 'vertColor');
-    gl.vertexAttribPointer(
-        positionAttribLocation, 
-        3, // Number of elements per attribute (x, y, z)
-        gl.FLOAT, // Type of elements
-        gl.FALSE, // Not normalized
-        6 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex (6 floats per vertex)
-        0 // Offset from the beginning of a single vertex to this attribute
-    );
-    gl.vertexAttribPointer(
-        colorAttribLocation,
-        3,
-        gl.FLOAT,
-        gl.FALSE,
-        6 * Float32Array.BYTES_PER_ELEMENT,
-        3 * Float32Array.BYTES_PER_ELEMENT
-    );
+    var linkAttribs = function(program) {
+        var positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
+        var colorAttribLocation = gl.getAttribLocation(program, 'vertColor');
+        var normalAttribLocation = gl.getAttribLocation(program, 'vertNormal');
+        gl.vertexAttribPointer(
+            positionAttribLocation, 
+            3, // Number of elements per attribute (x, y, z)
+            gl.FLOAT, // Type of elements
+            gl.FALSE, // Not normalized
+            9 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex (9 floats per vertex)
+            0 // Offset from the beginning of a single vertex to this attribute
+        );
+        gl.vertexAttribPointer(
+            colorAttribLocation,
+            3,
+            gl.FLOAT,
+            gl.FALSE,
+            9 * Float32Array.BYTES_PER_ELEMENT,
+            3 * Float32Array.BYTES_PER_ELEMENT
+        );
+        gl.vertexAttribPointer(
+            normalAttribLocation,
+            3,
+            gl.FLOAT,
+            gl.FALSE,
+            9 * Float32Array.BYTES_PER_ELEMENT,
+            6 * Float32Array.BYTES_PER_ELEMENT
+        );
 
-    gl.enableVertexAttribArray(positionAttribLocation);
-    gl.enableVertexAttribArray(colorAttribLocation);
+        gl.enableVertexAttribArray(positionAttribLocation);
+        gl.enableVertexAttribArray(colorAttribLocation);
+        gl.enableVertexAttribArray(normalAttribLocation);
+    };
+    linkAttribs(currentProgram);
 
-    // Get uniform locations for transformation matrices
-    var matWorldUniformLocation = gl.getUniformLocation(program, 'mWorld');
-    var matViewUniformLocation = gl.getUniformLocation(program, 'mView');
-    var matProjUniformLocation = gl.getUniformLocation(program, 'mProj');
+    // Get uniform locations for transformation matrices and lighting
+    var matWorldUniformLocation = gl.getUniformLocation(currentProgram, 'mWorld');
+    var matViewUniformLocation = gl.getUniformLocation(currentProgram, 'mView');
+    var matProjUniformLocation = gl.getUniformLocation(currentProgram, 'mProj');
+    var lightPositionLoc = gl.getUniformLocation(currentProgram, "uLightPosition");
+    var lightAmbientLoc = gl.getUniformLocation(currentProgram, "uLightAmbient");
+    var lightDiffuseLoc = gl.getUniformLocation(currentProgram, "uLightDiffuse");
+    var lightSpecularLoc = gl.getUniformLocation(currentProgram, "uLightSpecular");
+    var materialAmbientLoc = gl.getUniformLocation(currentProgram, "uMaterialAmbient");
+    var materialDiffuseLoc = gl.getUniformLocation(currentProgram, "uMaterialDiffuse");
+    var materialSpecularLoc = gl.getUniformLocation(currentProgram, "uMaterialSpecular");
+    var shininessLoc = gl.getUniformLocation(currentProgram, "uShininess");
+
+    // Set lighting and material properties
+    var lightPosition = [1.0, 1.0, 1.0, 1.0];
+    gl.uniform4fv(lightPositionLoc, lightPosition);
+    gl.uniform4fv(lightAmbientLoc, [0.2, 0.2, 0.2, 1.0]);
+    gl.uniform4fv(lightDiffuseLoc, [1.0, 1.0, 1.0, 1.0]);
+    gl.uniform4fv(lightSpecularLoc, [1.0, 1.0, 1.0, 1.0]);
+    gl.uniform4fv(materialAmbientLoc, [0.0, 1.0, 1.0, 1.0]);
+    gl.uniform4fv(materialDiffuseLoc, [0.0, 1.0, 1.0, 1.0]);
+    gl.uniform4fv(materialSpecularLoc, [1.0, 1.0, 1.0, 1.0]);
+    gl.uniform1f(shininessLoc, 80.0);
 
     // Define and set transformation matrices
     var worldMatrix = new Float32Array(16);
@@ -269,6 +339,66 @@ var InitDemo = function() {
         var maxZoom = -3;  
         zoom = minZoom + (zoomSlider.value / 100) * (maxZoom - minZoom);
     });
+
+    // Button event handlers for toggling shaders
+    var toggleShaderButton = document.getElementById('toggle-shader-button');
+    toggleShaderButton.addEventListener('click', function() {
+        currentProgram = (currentProgram === program) ? programAlt : program;
+        gl.useProgram(currentProgram);
+        linkAttribs(currentProgram);
+
+        // Update uniform locations after switching the program
+        matWorldUniformLocation = gl.getUniformLocation(currentProgram, 'mWorld');
+        matViewUniformLocation = gl.getUniformLocation(currentProgram, 'mView');
+        matProjUniformLocation = gl.getUniformLocation(currentProgram, 'mProj');
+        lightPositionLoc = gl.getUniformLocation(currentProgram, "uLightPosition");
+        lightAmbientLoc = gl.getUniformLocation(currentProgram, "uLightAmbient");
+        lightDiffuseLoc = gl.getUniformLocation(currentProgram, "uLightDiffuse");
+        lightSpecularLoc = gl.getUniformLocation(currentProgram, "uLightSpecular");
+        materialAmbientLoc = gl.getUniformLocation(currentProgram, "uMaterialAmbient");
+        materialDiffuseLoc = gl.getUniformLocation(currentProgram, "uMaterialDiffuse");
+        materialSpecularLoc = gl.getUniformLocation(currentProgram, "uMaterialSpecular");
+        shininessLoc = gl.getUniformLocation(currentProgram, "uShininess");
+
+        // Set lighting and material properties again
+        gl.uniform4fv(lightPositionLoc, lightPosition);
+        gl.uniform4fv(lightAmbientLoc, [0.2, 0.2, 0.2, 1.0]);
+        gl.uniform4fv(lightDiffuseLoc, [1.0, 1.0, 1.0, 1.0]);
+        gl.uniform4fv(lightSpecularLoc, [1.0, 1.0, 1.0, 1.0]);
+        gl.uniform4fv(materialAmbientLoc, [0.0, 1.0, 1.0, 1.0]);
+        gl.uniform4fv(materialDiffuseLoc, [0.0, 1.0, 1.0, 1.0]);
+        gl.uniform4fv(materialSpecularLoc, [1.0, 1.0, 1.0, 1.0]);
+        gl.uniform1f(shininessLoc, 80.0);
+
+        gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
+        gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
+        gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
+    });
+
+    // Event handlers for selecting and highlighting parts of the object
+    canvas.addEventListener('click', function(event) {
+        var rect = canvas.getBoundingClientRect();
+        var x = event.clientX - rect.left;
+        var y = event.clientY - rect.top;
+
+        var pixel = new Uint8Array(4);
+        gl.readPixels(x, rect.height - y, 1, 1, gl.RGBA, gl.UNSIGNED_BYTE, pixel);
+
+        if (pixel[3] > 0) {
+            // Highlight the selected part
+            console.log('Part selected: ', pixel);
+        }
+    });
+
+    // Dropdown event handler for switching visualizations
+    var visualizationDropdown = document.getElementById('visualization-dropdown');
+    visualizationDropdown.addEventListener('change', function() {
+        var visualization = visualizationDropdown.value;
+        // Change the visualization based on the selection
+        console.log('Switching to visualization: ', visualization);
+        // Implement different visualizations here
+    });
 };
 
 window.onload = InitDemo;
+
